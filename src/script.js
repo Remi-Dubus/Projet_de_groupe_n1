19afda19cbbb10e3ed20cab5db80c71275837c12
marco rojouan <marcoprojouan@outlook.fr>
2024-10-17 15:35:15 +0200

    feat : font family to all the css

--- src/script.js
+++ src/script.js
@@ -29,116 +29,116 @@ cards.innerHTML = "";
 // ];
 
 const newRestaurant = [
-	{
-		picture:
-			"https://images.pexels.com/photos/2098085/pexels-photo-2098085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-		name: "Dragon doré",
-		adresse: "9 rue Paul Louis Lande",
-		specialities: "Cuisine chinoise",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-premium/close-up-confit-canard-assiette-porcelaine-blanche-plat-traditionnel-francais-pattes-canard-cuites-lentement-garnies-feuilles-rogues-carte-visite-cuisine-francaise_124865-42395.jpg?w=826",
-		name: "Arcada",
-		adresse: "13 rue de la Rousselle",
-		specialities: "Terroir français",
-		hour: "Soir",
-		price: "+50€",
-		rating: "❤️❤️❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-premium/risotto-du-poivre-rouge-roti-ail-du-risotto-frais-photos1160jpg_995407-175251.jpg?w=826",
-		name: "Simeone Dell'Arte",
-		adresse: "10 place Camille Jullian",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
-	{
-		picture:
-			"https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
-		name: "Amore",
-		adresse: "11 place des Grands Hommes",
-		specialities: "Cuisine italienne",
-		hour: "Midi",
-		price: "25€-50€",
-		rating: "❤️❤️",
-	},
+  {
+    picture:
+      "https://images.pexels.com/photos/2098085/pexels-photo-2098085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name: "Dragon doré",
+    adresse: "9 rue Paul Louis Lande",
+    specialities: "Cuisine chinoise",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-premium/close-up-confit-canard-assiette-porcelaine-blanche-plat-traditionnel-francais-pattes-canard-cuites-lentement-garnies-feuilles-rogues-carte-visite-cuisine-francaise_124865-42395.jpg?w=826",
+    name: "Arcada",
+    adresse: "13 rue de la Rousselle",
+    specialities: "Terroir français",
+    hour: "Soir",
+    price: "+50€",
+    rating: "❤️❤️❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-premium/risotto-du-poivre-rouge-roti-ail-du-risotto-frais-photos1160jpg_995407-175251.jpg?w=826",
+    name: "Simeone Dell'Arte",
+    adresse: "10 place Camille Jullian",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
+  {
+    picture:
+      "https://img.freepik.com/photos-gratuite/pizza-melangee-tomates-champignons_140725-1818.jpg?t=st=1728988727~exp=1728992327~hmac=39b0cd2ed26b4677895ace6e53ab8902b960fd9ced800e5f798a1502039fbedf&w=740",
+    name: "Amore",
+    adresse: "11 place des Grands Hommes",
+    specialities: "Cuisine italienne",
+    hour: "Midi",
+    price: "25€-50€",
+    rating: "❤️❤️",
+  },
 ];
 
 // fonction pour créer de nouvelles cartes avec les infos présentes dans le tableau newRestaurant
@@ -196,19 +196,19 @@ const form = document.querySelector("#add-restaurant");
 newRestaurant.forEach((restaurant) => createCard(restaurant));
 
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	newRestaurant.push({
-		picture: e.target[8].value,
-		name: e.target[1].value,
-		adresse: e.target[2].value,
-		specialities: e.target[3].value,
-		hour: e.target[5].value,
-		price: e.target[4].value,
-		rating: e.target[6].value,
-	});
+  e.preventDefault();
+  newRestaurant.push({
+    picture: e.target[7].value,
+    name: e.target[1].value,
+    adresse: e.target[2].value,
+    specialities: e.target[3].value,
+    hour: e.target[5].value,
+    price: e.target[4].value,
+    rating: e.target[6].value,
+  });
 
-	// on ajoute seulement la carte générée par le formulaire, donc la dernière du tableau
-	createCard(newRestaurant.pop());
+  // on ajoute seulement la carte générée par le formulaire, donc la dernière du tableau
+  createCard(newRestaurant.pop());
 });
 
 /* bouton Haut de page */
@@ -217,47 +217,43 @@ const btnHautdePage = document.querySelector(".btnHautdePage");
 console.log(btnHautdePage);
 
 btnHautdePage.addEventListener("click", () => {
-	window.scrollTo({
-		top: 0,
-		left: 0,
-		behavior: "smooth",
-	});
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 });
 
-/* les filtres /
+//  les filtres
 
 let filterArray = [];
 
 function filter(arr) {
-/* destructuration du tableau /
-const { picture, name, adresse, specialities, hour, price, rating } = arr;
+  // destructuration du tableau
+  const { picture, name, adresse, specialities, hour, price, rating } = arr;
 
-/ recuperer le formulaire /
-const formulaireFilter = document.querySelector("#filter-form");
+  //  recuperer le formulaire
+  const formulaireFilter = document.querySelector("#filter-form");
 
-/ add listener /
-formulaireFilter.addEventListener("submit", function (event) {
+  //  add listener
+  formulaireFilter.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log(event);
 
-    / boucle verifiant la valeur selectionée /
+    // boucle verifiant la valeur selectionée
     for (let i = 1; i < event.target.length; i++) {
-        if (event.target[i].checked === true) {
-            let valueRestaurant = event.target[i].value
-            console.log(valueRestaurant)
-            for (let j = 0; j < arr.length; j++) {
-                if (event.target[i].value === arr[j].specialities) {
-                console.log(arr[j].specialities)
-
-                / filterArray.push(arr[j]);
-                console.log(filterArray);  */
-			}
-		} 
-	}
+      if (event.target[i].checked === true) {
+        let valueRestaurant = event.target[i].value;
+        console.log(valueRestaurant);
+        for (let j = 0; j < arr.length; j++) {
+          if (event.target[i].value === arr[j].specialities) {
+            console.log(arr[j].specialities) / filterArray.push(arr[j]);
+            console.log(filterArray);
+          }
+        }
+      }
+    }
+  });
 }
-})}
-
-
-
 
 // filter(newRestaurant)
